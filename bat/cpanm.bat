afcfc99b34582a2531cf82819e6e0d9990a6816a
Graham Ollis <plicease@cpan.org>
2017-01-16 15:10:30 -0500

    try another wrapper

--- bat/cpanm.bat
+++ bat/cpanm.bat
@@ -1,6 +1,6 @@
 @echo off
-echo this is cpanm wrapper
-perl -e "exec('cpanm', @ARGV)"
+echo cpanm wrapper
+perl -e "exec 'cpanm', @ARGV" -- %*
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_Exit_with_non_zero val 2> nul
 
