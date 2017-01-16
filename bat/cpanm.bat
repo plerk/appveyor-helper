b1fb9fe91ce2bb8763b6b5b25a653bd1079b49f0
Graham Ollis <plicease@cpan.org>
2017-01-16 15:12:58 -0500

    gibrush

--- bat/cpanm.bat
+++ bat/cpanm.bat
@@ -2,5 +2,4 @@
 echo cpanm wrapper
 perl -e "exec 'cpanm', @ARGV" -- %*
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
-if errorlevel 1 goto script_failed_so_Exit_with_non_zero val 2> nul
-
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
